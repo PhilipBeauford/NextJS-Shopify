0b3c6e7fa4e8eee01570da6a2ccf1c36adae435e
Philip Beauford <PhilipBeauford@github.com>
2021-10-11 13:26:23 -0700

    updates

--- components/Hero.js
+++ components/Hero.js
@@ -46,8 +46,8 @@ export default function Hero() {
           sizes="100vw" 
           src="./Saly_19.png" 
           decoding="async" 
-          style={{position:"absolute", top:0, left:0, bottom:130, right:0, boxSizing:"border-box", padding:0, border:"none", margin:"auto", display:"block", width:0, 
-          height:0, minWidth:"100%", maxWidth:"100%", minHeight:"100%", maxHeight:"100%", objectFit:"cover" }} />
+          style={{position:"absolute", top:0, left:0, bottom:0, right:0, boxSizing:"border-box", padding:0, border:"none", margin:"auto", display:"block", width:0, 
+          height:0, minWidth:"100%", maxWidth:"100%", minHeight:"100%", maxHeight:"100%", objectFit:"contain" }} />
       </div>
   </div>
 </div> 
