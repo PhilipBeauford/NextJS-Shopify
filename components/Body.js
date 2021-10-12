c2432ea62e506634a4860278afb1c7f297cabeab
Philip Beauford <PhilipBeauford@github.com>
2021-10-12 09:22:03 -0700

    updated main

--- components/Body.js
+++ components/Body.js
@@ -47,37 +47,34 @@ export default function Body() {
                         </div>
                     </div>
 
-
-
                     <div className="w-full h-full p-8 rounded-3xl shadow-2xl bg-white lg:relative lg:-top-40">
                         <span className="text-4xl">
                             💰
                         </span>
-                            <h3 className="font-bold text-2xl">
-                                Create over
-                                <span className="decoration-clone text-transparent bg-clip-text bg-gradient-to-r from-purple-400 via-pink-500 to-red-500">
-                                    &nbsp;$50,000&nbsp;
-                                </span>
-                                in value for yourself or your clients!
-                            </h3>
-                            <p className="text-gray-500 py-5">
-                            "Let’s start with one of the most important factors: cost. The pricing for an enterprise headless project could cost anywhere from 
-                            $50,000 to $500,000, plus annual maintenance costs, depending on the functionality, supporting tools, and level of customization." -
-                                <a className="text-green-600 font-bold" target="_blank" rel="noreferrer" href="https://www.shopify.com/enterprise/headless-commerce">
-                                    Shopify
-                                </a> 
-                            </p>
+                        <h3 className="font-bold text-2xl">
+                            Create over
 
-                            <div className="relative h-96 w-full">
-                                <div style= {{display:"block", overflow:"hidden", position:"absolute", top:0, left:0, bottom:0, right:0, boxSizing:"border-box", margin:0}} >
-                                    <img alt="Hero Image" sizes="100vw" src="./worldwide.png" decoding="async" style= {{position:"absolute",top:0,left:0,bottom:0, right:0, 
-                                    boxSizing:"border-box", padding:0, border:"none", margin:"auto", display:"block", width:0, height:0, minWidth:"100%", maxWidth:"100%", minHeight:"100%",
-                                    maxHeight:"100%", objectFit:"contain" }} />
-                                </div>
+                            <span className="decoration-clone text-transparent bg-clip-text bg-gradient-to-r from-purple-400 via-pink-500 to-red-500">
+                                &nbsp;$50,000&nbsp;
+                            </span>
+
+                            in value for yourself or your clients!
+                        </h3>
+                        <p className="text-gray-500 py-5">
+                                "Let’s start with one of the most important factors: cost. The pricing for an enterprise headless project could cost anywhere from 
+                                $50,000 to $500,000, plus annual maintenance costs, depending on the functionality, supporting tools, and level of customization." -
+                            <a className="text-green-600 font-bold" target="_blank" rel="noreferrer" href="https://www.shopify.com/enterprise/headless-commerce">
+                                Shopify
+                            </a> 
+                        </p>
+                        <div className="relative h-96 w-full">
+                            <div style= {{display:"block", overflow:"hidden", position:"absolute", top:0, left:0, bottom:0, right:0, boxSizing:"border-box", margin:0}} >
+                                <img alt="Hero Image" sizes="100vw" src="./worldwide.png" decoding="async" style= {{position:"absolute",top:0,left:0,bottom:0, right:0, 
+                                boxSizing:"border-box", padding:0, border:"none", margin:"auto", display:"block", width:0, height:0, minWidth:"100%", maxWidth:"100%", minHeight:"100%",
+                                maxHeight:"100%", objectFit:"contain" }} />
                             </div>
+                        </div>
                     </div>
-
-
 
                     <div className="w-full h-full p-8 rounded-3xl shadow-2xl bg-white">
                         <span className="text-4xl">
@@ -113,7 +110,7 @@ export default function Body() {
                             </span>
                         </h3>
                         <p className="text-gray-500 py-5">
-                            I only use cutting edge technologies and always write clean, efficient code.
+                            I use cutting edge technologies and always write clean, efficient code.
                         </p>
                         <div className="relative h-96 w-full">
                             <div style={{ display:"block", overflow:"hidden", position:"absolute", top:0, left:0, bottom:0, right:0, boxSizing:"border-box", margin:0 }}>
@@ -122,12 +119,48 @@ export default function Body() {
                         </div>
                     </div>
 
+                
+
                 </div>
             </div>
 
+                <div className="max-w-xl mx-auto mt-10 sm:pt-0 sm:my-auto lg:pt-0 pb-0 px-0 sm:pb-12 sm:px-6 lg:max-w-7xl lg:px-8 lg:-mt-20 relative -top-10 lg:-top-14">
+                    <h3 className="mb-6 font-sans text-4xl font-bold tracking-tight text-center lg:text-right text-white sm:text-5xl lg:-pt-12 lg:-w-1/2 lg:mb-0">
+                        Sounds
+                        <span className="bg-clip-text text-transparent bg-gradient-to-r from-purple-400 via-pink-500 to-red-400">
+                             &nbsp;great&nbsp; 
+                        </span>
+                        right?
+                    </h3>
+                </div>
+            
         </div>
     )
 }
 
+/*his
+max-w-xl 
+mx-auto 
+mt-16 
+pt-12 
+pb-8 
+px-4 
+sm:mt-48 
+sm:pb-12 
+sm:px-6 
+lg:max-w-7xl 
+lg:px-8
+
+<h3>
+sm:text-5xl 
+lg:pt-12 
+lg:w-1/2 
+lg:mb-0
+*/
 
 
+//mine
+//block max-w-xl mx-auto mt-16 sm:pt-12 lg:pt-0 pb-0 px-0 sm:mt-20 sm:pb-0 sm:px-6 lg:max-w-7xl lg:px-8
+
+//md:absolute sm:relative right-36 mb-4 md:-mt-64 sm:-mt-5 font-sans text-4xl font-bold tracking-tight sm:text-center 
+//lg:text-right text-white sm:text-5xl lg:pt-12 lg:w-1/2 lg:mb-0
